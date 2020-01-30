02dafd6dfa0e4ceafc2c3d7c6a26ea52fbd65c08
Henry Hunt <henryhunt.dev@gmail.com>
2020-01-30 22:42:10 +0000

    Converted index page to use AJAX

---
+++ data/get-completed-sessions.php
@@ -0,0 +1,27 @@
+<?php
+date_default_timezone_set("UTC");
+include_once("../resources/routines/helpers.php");
+include_once("../resources/routines/config.php");
+
+$setup_error = false;
+$config = new Config();
+if (!$config->load_config("../config.ini"))
+    $setup_error = true;
+$db_connection = database_connection($config);
+if (!$db_connection)
+    $setup_error = true;
+if ($setup_error) { echo "false"; exit(1); }
+
+
+// Gets the completed sessions (exact opposite of the active sessions)
+$QUERY = "SELECT session_id, name, " .
+    "(SELECT start_time FROM session_nodes WHERE session_id = sessions.session_id ORDER BY start_time ASC LIMIT 1) AS start_time, " .
+    "(SELECT end_time FROM session_nodes WHERE session_id = sessions.session_id ORDER BY end_time DESC LIMIT 1) AS end_time, " .
+    "(SELECT COUNT(*) FROM session_nodes WHERE session_id = sessions.session_id) AS node_count " .
+    "FROM sessions HAVING NOT (start_time IS NOT NULL " .
+    "AND (start_time > NOW() OR end_time IS NULL OR NOW() BETWEEN start_time AND end_time)) ORDER BY name";
+
+$result = query_database($db_connection, $QUERY, NULL);
+if ($result === false) { echo "false"; exit(1); }
+
+echo json_encode($result);
---
+++ resources/scripts/index.js
@@ -0,0 +1,100 @@
+var ERROR_HTML = "<div class='message_item'><span>Error Getting Data</span></div>";
+var NO_DATA_HTML = "<div class='message_item'><span>Nothing Here</span></div>";
+
+window.onload = function()
+{
+    // Load currently active sessions
+    $.ajax({
+        dataType: "json", url: "data/get-active-sessions.php",
+        success: function (data)
+        {
+            if (data !== false)
+            {
+                var format = `<div class='group_item'><a href='session.php?id={0}'>
+                    <span>{1}</span><br><span>{2}</span><span>{3}</span></a></div>`;
+
+                if (data !== null)
+                {
+                    var html = "";
+                    for (var i = 0; i < data.length; i++)
+                    {
+                        var session_dates = "From ";
+                        var start_time = moment.utc(data[i]["start_time"], "YYYY-MM-DD HH:mm:ss");
+                        session_dates += start_time.tz("Europe/London").format("DD/MM/YYYY");
+
+                        if (data[i]["end_time"] !== null)
+                        {
+                            var end_time = moment.utc(data[i]["end_time"], "YYYY-MM-DD HH:mm:ss");
+                            session_dates += " to ";
+                            session_dates += end_time.tz("Europe/London").format("DD/MM/YYYY");
+                        } else session_dates += ", indefinitely";
+
+                        if (data[i]["node_count"] != 1)
+                            var node_count = data[i]["session_id"] + " Sensor Nodes";
+                        else var node_count = "1 Sensor Node";
+
+                        html += format.format(data[i]["session_id"],data[i]["name"],
+                            session_dates, node_count);
+                    }
+
+                    $("#active_sessions").append(html);
+                } else $("#active_sessions").append(NO_DATA_HTML);
+            } else $("#active_sessions").append(ERROR_HTML);
+
+            $("#active_sessions_group").css("display", "block");
+        },
+
+        error: requestError = () => {
+            $("#active_sessions").append(ERROR_HTML);
+            $("#active_sessions_group").css("display", "block");
+        }
+    });
+
+    // Load completed sessions
+    $.ajax({
+        dataType: "json", url: "data/get-completed-sessions.php",
+        success: function (data)
+        {
+            if (data !== false)
+            {
+                var format = `<div class='group_item'><a href='session.php?id={0}'>
+                    <span>{1}</span><br><span>{2}</span><span>{3}</span></a></div>`;
+
+                if (data !== null)
+                {
+                    var html = "";
+                    for (var i = 0; i < data.length; i++)
+                    {
+                        var session_dates = "";
+                        if (data[i]["start_time"] !== null)
+                        {
+                            session_dates += "From ";
+                            var start_time = moment.utc(data[i]["start_time"], "YYYY-MM-DD HH:mm:ss");
+                            session_dates += start_time.tz("Europe/London").format("DD/MM/YYYY");
+
+                            var end_time = moment.utc(data[i]["end_time"], "YYYY-MM-DD HH:mm:ss");
+                            session_dates += " to ";
+                            session_dates += end_time.tz("Europe/London").format("DD/MM/YYYY");
+                        }
+
+                        if (data[i]["node_count"] != 1)
+                            var node_count = data[i]["session_id"] + " Sensor Nodes";
+                        else var node_count = "1 Sensor Node";
+
+                        html += format.format(data[i]["session_id"], data[i]["name"],
+                            session_dates, node_count);
+                    }
+
+                    $("#completed_sessions").append(html);
+                } else $("#completed_sessions").append(NO_DATA_HTML);
+            } else $("#completed_sessions").append(ERROR_HTML);
+
+            $("#completed_sessions_group").css("display", "block");
+        },
+
+        error: requestError = () => {
+            $("#completed_sessions").append(ERROR_HTML);
+            $("#completed_sessions_group").css("display", "block");
+        }
+    });
+};
